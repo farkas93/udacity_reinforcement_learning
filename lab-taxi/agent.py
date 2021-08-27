f3845307ce5724b4c9496e4f72730009660928b9
farkas93 <zsombor.kalotay@gmx.ch>
2021-08-27 11:16:24 +0200

    solve taxi mini project (score > 9.1)

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -12,6 +12,9 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = 0.005
+        self.gamma = 1.0
+        self.alpha = .05
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,8 +27,36 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        
+        if np.random.random() > self.epsilon:
+            return np.argmax(self.Q[state])
+        else:
+            return np.random.choice(np.arange(self.nA))
+    
+    def get_probs(self, state):
+        """ Given the state, return the probabilities for chosing any of the possible actions.
 
+        Params
+        ======
+        - state: the current state of the environment.
+
+        Returns
+        =======
+        - probs: list of float type probabilities for chosing each of the possible actions.
+        """
+        probs = np.ones(self.nA) * self.epsilon / self.nA
+        max_ind = np.argmax(self.Q[state])
+        probs[max_ind] += (1.0 - self.epsilon)
+        return probs
+    
+    def q_learn(self, next_state):
+        next_action = np.argmax(self.Q[next_state])
+        return self.Q[next_state][next_action]
+        
+    def expected_sarsa(self, next_state):
+        return np.dot(self.get_probs(next_state),self.Q[next_state])
+        
+    
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
@@ -37,4 +68,7 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        q_cur = self.Q[state][action]
+        q_update = self.q_learn(next_state)
+        self.Q[state][action] = q_cur + self.alpha * (reward + self.gamma*q_update - q_cur)
+        
