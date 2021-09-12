45bbb94a27472d30983d29a0747ce7f66cbe0973
kalotayz <zsombor.kalotay@gmx.ch>
2021-09-12 18:09:11 +0200

    upload first files for project 1

--- p1_navigation/dqn_agent.py
+++ p1_navigation/dqn_agent.py
@@ -84,9 +84,7 @@ class Agent():
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
-        
-        ## TODO: compute and minimize the loss
-        "*** YOUR CODE HERE ***"
+
         # Get max predicted Q values (for next states) from target model
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states 
